0cbc7b8c4d6dd7fa9e61bbec475de0138b1fe4f4
Artem Shitov <inbox@artemshitov.ru>
2013-12-07 18:17:38 +0400

    Flatten a list

---
+++ 02-99Problems/07-Flatten.hs
@@ -0,0 +1,10 @@
+data NestedList a = Elem a | List [NestedList a]
+
+flatten :: NestedList a -> [a]
+flatten (Elem x) = [x]
+flatten (List []) = []
+flatten (List (x:xs)) = flatten x ++ flatten (List xs)
+
+flatten' :: NestedList a -> [a]
+flatten' (Elem x) = [x]
+flatten' (List xs) = concatMap flatten' xs
